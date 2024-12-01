ec8448eb3c3afb149b411be0cb6ff458069716f2
Alexander Berntsson <alexande.berntsson@gmail.com>
2024-12-01 06:08:59 +0100

    day 1

--- day1/main.py
+++ day1/main.py
@@ -2,7 +2,30 @@
 
 import sys
 import os
+from collections import defaultdict
 
-content = sys.stdin.read()
+content = open("input").read()
+groups = content.split('\n\n')
+lines = content.splitlines()
 
-print(content)
+l = []
+r = []
+
+for line in lines:
+    a, b = map(int, line.split())
+    l.append(a)
+    r.append(b)
+
+sm = 0
+l = sorted(l)
+r = sorted(r)
+for i in range(len(l)):
+    sm += abs(l[i] - r[i])
+print(f'part 1: {sm}')
+
+sm = 0
+for i in range(len(l)):
+    a = l[i]
+    sm += a*r.count(a)
+
+print(f'part 2: {sm}')
